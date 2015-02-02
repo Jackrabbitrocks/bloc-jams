6a384fcee2770792833b4bd1687612e1d7c7c594
Jackrabbit <jackrabbitrocks@gmail.com>
2015-02-02 00:26:50 +0000

    Added player bar to album view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -123,37 +123,41 @@ var albumPicasso = {
  	]
  };
 
-var currentlyPlayingSong = null;
+ var currentlyPlayingSong = null;
 
  var createSongRow = function(songNumber, songName, songLength) {
  	var template =
  	'<tr>'
- 	 + '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
- 	+ '  <td class="col-md-9">' + songName + '</td>'
- 	+ '  <td class="col-md-2">' + songLength + '</td>'
- 	+ '</tr>'
- 	;
+   + '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+   + '  <td class="col-md-9">' + songName + '</td>'
+   + '  <td class="col-md-2">' + songLength + '</td>'
+   + '</tr>'
+   ;
 
  	// Instead of returning the row immediately, we'll attach hover
   // functionality to it first.
-   var $row = $(template);
- 
-   var onHover = function(event) {
-     var songNumberCell = $(this).find('.song-number');
-     var songNumber = songNumberCell.data('song-number');
-    if (songNumber !== currentlyPlayingSong) {
-      songNumberCell.html('<a class="album-song-button"><i class="fa fa-play"></i></a>');
-    }
-   };
- 
-   var offHover = function(event) {
-     var songNumberCell = $(this).find('.song-number');
-     if (songNumber !== currentlyPlayingSong) {
-      songNumberCell.html(songNumber);
-    }
-   };
+  var $row = $(template);
 
-   if (currentlyPlayingSong !== null) {
+  var onHover = function(event) {
+   var songNumberCell = $(this).find('.song-number');
+   var songNumber = songNumberCell.data('song-number');
+   if (songNumber !== currentlyPlayingSong) {
+    songNumberCell.html('<a class="album-song-button"><i class="fa fa-play"></i></a>');
+  }
+};
+
+var offHover = function(event) {
+ var songNumberCell = $(this).find('.song-number');
+ if (songNumber !== currentlyPlayingSong) {
+  songNumberCell.html(songNumber);
+}
+};
+
+// Toggle the play, pause, and song number based on which play/pause button we clicked on.
+  var clickHandler = function(event) {
+    var songNumber = $(this).data('song-number');
+
+     if (currentlyPlayingSong !== null) {
        // Revert to song number for currently playing song because user started playing new song.
        currentlyPlayingCell = $('.song-number[data-song-number="' + currentlyPlayingSong + '"]');
        currentlyPlayingCell.html(currentlyPlayingSong);
@@ -171,8 +175,7 @@ var currentlyPlayingSong = null;
      }
    };
 
-
- $row.find('.song-number').click(clickHandler);
+   $row.find('.song-number').click(clickHandler);
    $row.hover(onHover, offHover);
    return $row;
  };
@@ -205,7 +208,7 @@ var currentlyPlayingSong = null;
    	var $newRow = createSongRow(i + 1, songData.name, songData.length);
    	$songList.append($newRow);
    }
-}
+ }
 
   // This 'if' condition is used to prevent the jQuery modifications
  // from happening on non-Album view pages.
@@ -215,7 +218,7 @@ var currentlyPlayingSong = null;
    $(document).ready(function() {
    	changeAlbumView(albumPicasso);
    });
-}
+ }
 });
 
 ;require.register("scripts/app", function(exports, require, module) {

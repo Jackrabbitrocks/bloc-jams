17e16a9fb8ec50f203ad7336eaf6e8e8e219c9c4
Jackrabbit <jackrabbitrocks@gmail.com>
2015-02-03 18:45:55 +0000

    Converted Bloc Jams to a single page application

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -270,41 +270,48 @@ function consoleUpdate (event) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-// require("./landing");
-// require("./collection");
-// require("./album");
-// require("./profile");
+blocJams = angular.module('BlocJams', ['ui.router']);
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+ $locationProvider.html5Mode(true);
+
+ $stateProvider.state('landing', {
+   url: '/',
+   controller: 'Landing.controller',
+   templateUrl: '/templates/landing.html'
+ });
+}]);
+
+blocJams.controller('Landing.controller', ['$scope', function($scope){
   $scope.subText = "Turn down for what?";
   $scope.subTextClicked = function() {
-  	$scope.subText += "?";
+    $scope.subText += "?";
   };
 
   var randoAlbums = [
-    '/images/album-placeholders/album-1.jpg',
-     '/images/album-placeholders/album-2.jpg',
-     '/images/album-placeholders/album-3.jpg',
-     '/images/album-placeholders/album-4.jpg',
-     '/images/album-placeholders/album-5.jpg',
-     '/images/album-placeholders/album-6.jpg',
-     '/images/album-placeholders/album-7.jpg',
-     '/images/album-placeholders/album-8.jpg',
-     '/images/album-placeholders/album-9.jpg',
-     ];
+  '/images/album-placeholders/album-1.jpg',
+  '/images/album-placeholders/album-2.jpg',
+  '/images/album-placeholders/album-3.jpg',
+  '/images/album-placeholders/album-4.jpg',
+  '/images/album-placeholders/album-5.jpg',
+  '/images/album-placeholders/album-6.jpg',
+  '/images/album-placeholders/album-7.jpg',
+  '/images/album-placeholders/album-8.jpg',
+  '/images/album-placeholders/album-9.jpg',
+  ];
 
   $scope.randoAlbm = function() {
     $scope.albumURLs = shuffle(randoAlbums);
-  }
+  };
 
   $scope.albumURLs = shuffle(randoAlbums);
-   
- }]);
 
-function shuffle(o){ //v1.0
+  function shuffle(o){ //v1.0
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
+      return o;
+  }
+
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
